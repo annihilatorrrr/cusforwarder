fc231628ac12fd8700c5504426ee7e6853313cf5
ÁÑÑÍHÌLÅTØR SPÄRK <75305464+annihilatorrrr@users.noreply.github.com>
2023-10-31 11:35:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ except Exception:
     exit("Unable to connect with redisdb !")
 
 
-def replaceshits(text: str):
+async def replaceshits(text: str):
     for i in findall(r"(@[A-Za-z0-9_]+( |$|\b))", text):
         text = text.replace(i[0], "")
     for i in findall(r"((http(s)?://)?(t|telegram)\.(me|dog)/[A-Za-z0-9_]+( |$|\b))", text):
         text = text.replace(i[0], "")
+    for x in await REDIS.smembers("words"):
+        text = text.replace(x, "")
     return text
 
 
@@ -33,9 +35,46 @@ async def startb(_, m: Message):
     return
 
 
+@pbot.on_message(command("addchannel") & private)
+async def addchannel(_, m: Message):
+    args = m.command[1:]
+    if len(args) >= 2:
+        try:
+            source = await c.get_chat(args[0])
+        except RPCError as e:
+            await m.reply_text(e.MESSAGE)
+            return
+        try:
+            desti = await c.get_chat(args[1])
+        except RPCError as e:
+            await m.reply_text(e.MESSAGE)
+            return
+        try:
+            await ubot.join_chat(desti.invite_link)
+        except:
+            pass
+    else:
+        await m.reply_text("Provide like: /addchannel sourcechatid destinationchatid")
+    return
+
+
+
+async def worker(c: ubot, m: Message):
+    if data := await REDIS.get(m.chat.id):
+        capt = await replaceshits(m.caption.html) if m.caption else None
+        try:
+            await m.copy(int(data), caption=capt)
+        except FloodWait as fe:
+            await sleep(fe.value + 1)
+            await m.copy(int(data), caption=capt)
+        except Exception:
+            pass
+    return
+
+
 @ubot.on_message(channel)
 async def forward(c: ubot, m: Message):
-    await m.reply_text("I'm alive!\nCustom forwarder bot Made by @annihilatorrrr !")
+    c.loop.create_task(worker(c, m))
     return
 
 
